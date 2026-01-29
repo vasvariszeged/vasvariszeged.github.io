e35f6c3ca5c177e32bc4206f41725e5912a7a68e
vasvariszeged <redai.david@vasvari.org>
2026-01-29 10:34:41 +0100

    New upload

--- assets/index.html-042afbda.js
+++
@@ -1,1 +0,0 @@
-const a=JSON.parse('{"key":"v-22b355e0","path":"/csharp/basic/","title":"Az alapok 1. rész","lang":"en-US","frontmatter":{"icon":"fa-solid fa-dungeon","category":["C#"],"seo":false,"head":[]},"headers":[],"git":{},"readingTime":{"minutes":0.26,"words":79},"filePathRelative":"csharp/basic/README.md"}');export{a as data};
--- assets/index.html-5ed632f9.js
+++
@@ -1,1 +0,0 @@
-const a=JSON.parse('{"key":"v-33b76508","path":"/csharp/basic2/","title":"Az alapok 2. rész","lang":"en-US","frontmatter":{"icon":"fa-solid fa-wand-sparkles","category":["C#"],"seo":false,"head":[]},"headers":[],"git":{},"readingTime":{"minutes":0.16,"words":47},"filePathRelative":"csharp/basic2/README.md"}');export{a as data};
